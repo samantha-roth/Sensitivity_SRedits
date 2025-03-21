df60d776f56d5913860453da64cfefb56a9557a9
Samantha M. Roth <41164073+samantha-roth@users.noreply.github.com>
2025-03-21 14:12:30 -0500

    new ways to compare methods

--- 11_CompareTotalTimes.R
+++ 11_CompareTotalTimes.R
@@ -6,6 +6,7 @@ rm(list = ls())
 graphics.off()
 
 source("0_library.R")
+source("extra_functions.R")
 
 # Load the required package for plotting
 library(plot.matrix)
@@ -26,35 +27,50 @@ Time_AKMCS <- matrix(NA,nrow = length(tested_D),ncol = length(tested_eval_time))
 Time_BASS <- matrix(NA,nrow = length(tested_D),ncol = length(tested_eval_time))
 
 # Load all related results for each test scenario
-for (i in 1:(length(D))) {
-  folder <- paste("./Ranking_Data/",tested_D[i],sep="")
-  
-  # General:
-  load(paste(folder,"/avg_eval_time",sep=""))
+for (i in 1:(length(D)-1)) {
+  folder <- paste0("./Ranking_Data/",tested_D[i])
   
   # Sobol:
   load(paste0(folder,"/Sobol/T_Sobol"))
   load(paste0(folder,"/Sobol/T_check_Sobol"))
-  load(paste0(folder,"/all_eval_times"))
-  load(paste(folder,"/Sobol/S_Sobol",sep=""))
+  load(paste0(folder,"/Sobol/avg_eval_time"))
+  load(paste0(folder,"/Sobol/S_Sobol"))
   Sobol_convergesize <- S$C
-
   
   # BASS:
   load(paste0(folder, "/BASS/T_BASS"))
   load(paste0(folder, "/BASS/T_pred_BASS"))
+  load(paste0(folder, "/BASS/T_LHS_BASS"))
   load(paste0(folder, "/BASS/T_check_BASS"))
   load(paste0(folder,"/BASS/T_BASSSobol"))
   load(paste0(folder,"/BASS/S_BASS"))
   load(paste0(folder,"/BASS/BASS_size"))
+
+  #get all the sizes tested in standard Sobol'
+  d<-tested_D_num[i]
+  idx <- closest_greater(tot_size,S$C)
+  N<- floor(tot_size[idx]/(d+2+d*(d-1)/2))
+  if(S$C==N*(d+2+d*(d-1)/2)){
+    all_sizes<- size_checked(idx,d)
+  }else{
+    print("something is wrong")
+    break
+  }
   
   # Calculation of the computational time in each scenario
   # Sensitivity analysis time + model evaluation adjusted time + emulation time
   for (j in 1:length(tested_eval_time)) {
-    Time_Sobol[i,j] <- sum(T_Sobol)- sum(all_eval_times) + sum(T_check_Sobol) + tested_eval_time[j]*Sobol_convergesize
-    Time_BASS[i,j] <- sum(T_BASS)+ sum(T_pred_BASS) + tested_eval_time[j]*BASS_size + sum(T_BASSSobol) + sum(T_check_BASS)
+    tot_Sobol_eval_time<-0 
+    for(m in 1:length(all_sizes)){
+      tot_Sobol_eval_time<- tot_Sobol_eval_time + tested_eval_time[j]*all_sizes[m]
+    }
+    Time_Sobol[i,j] <- sum(T_Sobol)- length(T_Sobol)*avg_eval_time + sum(T_check_Sobol) + tot_Sobol_eval_time
+    Time_BASS[i,j] <- sum(T_LHS_BASS) + sum(T_BASS)+ sum(T_pred_BASS) + tested_eval_time[j]*BASS_size + sum(T_BASSSobol) + sum(T_check_BASS)
   }
 }
+
+save(Time_Sobol,file=paste0("./Ranking_Data/Time_Sobol_node0"))
+save(Time_BASS,file=paste0("./Ranking_Data/Time_BASS_node0"))
 
 for(i in 1:(length(D)-1)){
   folder <- paste0("./Ranking_Data/",tested_D[i])
@@ -62,6 +78,7 @@ for(i in 1:(length(D)-1)){
   # Kriging:
   load(paste0(folder,"/Kriging/T_Kriging"))
   load(paste0(folder,"/Kriging/T_pred_Kriging"))
+  load(paste0(folder,"/Kriging/T_LHS_Kriging"))
   load(paste0(folder,"/Kriging/T_KrigingSobol"))
   load(paste0(folder,"/Kriging/T_check_Kriging"))
   load(paste0(folder,"/Kriging/Kriging_size"))
@@ -81,10 +98,13 @@ for(i in 1:(length(D)-1)){
   # Calculation of the computational time in each scenario
   # Sensitivity analysis time + model evaluation adjusted time + emulation time
   for (j in 1:length(tested_eval_time)) {
-    Time_Kriging[i,j] <- sum(T_Kriging) + sum(T_pred_Kriging) + tested_eval_time[j]*Kriging_size + sum(T_KrigingSobol) + sum(T_check_Kriging)
+    Time_Kriging[i,j] <- sum(T_LHS_Kriging) + sum(T_Kriging) + sum(T_pred_Kriging) + tested_eval_time[j]*Kriging_size + sum(T_KrigingSobol) + sum(T_check_Kriging)
     Time_AKMCS[i,j] <- sum(T_AKMCS) + sum(T_pred_AKMCS) + tested_eval_time[j]*AKMCS_size + sum(T_AKMCSSobol) + sum(T_check_AKMCS)
   }
 }
+
+save(Time_Kriging,file=paste0("./Ranking_Data/Time_Kriging_node0"))
+save(Time_AKMCS,file=paste0("./Ranking_Data/Time_AKMCS_node0"))
 
 Time_AKMCS[length(D),]<- NA
 Time_Kriging[length(D),]<- NA
---
+++ 14_howBetterIsBest.R
@@ -0,0 +1,126 @@
+# how different is the "best" for all seeds from the other approaches where it is best?
+
+rm(list=ls())
+graphics.off()
+
+source("0_library.R")
+source("extra_functions.R")
+
+# Load the required package for plotting
+library(plot.matrix)
+library(RColorBrewer)
+
+# Tested dimension, method names, and evaluation time
+tested_D_num <- c(2,5,10,15,20,30)
+tested_D <- c("2D","5D","10D","15D","20D","30D")
+tested_M <- c("Kriging","AKMCS","BASS")
+tested_eval_time <- c(0.001,0.01,0.1,1,10,60,600,3600,3600*10)
+# Label of evaluation time
+eval_time_lab <- c("1ms","10ms","0.1s","1s","10s","1min","10min","1h","10h")
+
+
+load(paste0("./Ranking_Data/Summary_Time_Sobol"))
+load(paste0("./Ranking_Data/Summary_Time_BASS"))
+load(paste0("./Ranking_Data/Summary_Time_Kriging"))
+load(paste0("./Ranking_Data/Summary_Time_AKMCS"))
+
+load("./Ranking_Data/textMat_maxSobol")
+load("./Ranking_Data/textMat_maxBASS")
+load("./Ranking_Data/textMat_maxKriging")
+load("./Ranking_Data/textMat_maxAKMCS")
+
+
+################################################################################
+whichSobolBest<- which(textMat_maxSobol=="Sobol",arr.ind=FALSE)
+
+Mean_Diff_BASS<- rep(NA, length(whichSobolBest))
+Mean_Diff_Kriging<- rep(NA, length(whichSobolBest))
+Mean_Diff_AKMCS<- rep(NA, length(whichSobolBest))
+
+Mean_Diff_BASS<- Mean_Time_BASS[whichSobolBest]-Mean_Time_Sobol[whichSobolBest]
+Mean_Diff_Kriging<- Mean_Time_Kriging[whichSobolBest]-Mean_Time_Sobol[whichSobolBest]
+Mean_Diff_AKMCS<- Mean_Time_AKMCS[whichSobolBest]-Mean_Time_Sobol[whichSobolBest]
+
+Mean_Time_Sobol_Best<- Mean_Time_Sobol[whichSobolBest]
+
+Mean_Diff_Min<- rep(NA,length(whichSobolBest))
+min_method<- rep(NA,length(whichSobolBest))
+for(i in 1:length(whichSobolBest)){
+  Mean_Diff_Min[i]<- min(Mean_Diff_AKMCS[i],Mean_Diff_BASS[i],Mean_Diff_Kriging[i])
+  if(Mean_Diff_Min[i]==Mean_Diff_BASS[i]) min_method[i]<- "BASS"
+  if(Mean_Diff_Min[i]==Mean_Diff_Kriging[i]) min_method[i]<- "Kriging"
+  if(Mean_Diff_Min[i]==Mean_Diff_AKMCS[i]) min_method[i]<- "AKMCS"
+}
+
+
+pct_diff_min<- Mean_Diff_Min/Mean_Time_Sobol_Best
+pct_diff_min
+
+
+Mean_Time_Sobol_Best_min<- Mean_Time_Sobol_Best/60
+Mean_Time_Sobol_Best_hr<- Mean_Time_Sobol_Best/3600
+Mean_Time_Sobol_Best_day<- Mean_Time_Sobol_Best_hr/24
+Mean_Time_Sobol_Best_yr<- Mean_Time_Sobol_Best_day/365
+
+Mean_Time_Sobol_Best_hr
+
+#1ms
+#2D: not meaningful- the mean difference is less than a minute and both approaches take <1 min
+#5D: not likely meaningful- difference is about 3 minutes, but Sobol only takes seconds
+#10D: meaningful- difference is about half an hour, Sobol only takes seconds
+#15D: very meaningful- difference is about 7.5 hours, Sobol only takes seconds
+#20D: very meaningful- difference is almost 2 days, Sobol only takes seconds
+#30D: add
+
+#10ms
+#2D: not meaningful- the mean difference is less than a minute and both approaches take <1 min
+#5D: not likely meaningful- difference is about 3 minutes, but Sobol only takes seconds
+#10D: meaningful- difference is about half an hour, Sobol only takes 1.5 min
+#15D: very meaningful- difference is about 7.5 hours, Sobol only takes less than a minute
+#20D: very meaningful- difference is almost 2 days (41 hours), Sobol only takes about 1.5 min
+#30D: add
+
+#.1s
+#15D: very meaningful- difference is about 7.5 hours, Sobol only takes 6 min
+#20D: very meaningful- difference is almost 2 days (41 hours), Sobol only takes about 11 min
+#30D: add
+
+#1s
+#20: very meaningful- difference is (40 hours), Sobol takes almost 2 hours
+#30D: add
+
+Mean_Diff_Min_min<- Mean_Diff_Min/60
+Mean_Diff_Min_hr<- Mean_Diff_Min/3600
+Mean_Diff_Min_day<- Mean_Diff_Min_hr/24
+Mean_Diff_Min_yr<- Mean_Diff_Min_day/365
+
+Mean_Diff_Min_hr
+
+Mean_Diff_BASS_hr<- Mean_Diff_BASS/3600
+Mean_Diff_BASS_day<- Mean_Diff_BASS_hr/24
+Mean_Diff_BASS_yr<- Mean_Diff_BASS_day/365
+
+Mean_Diff_Kriging_hr<- Mean_Diff_Kriging/3600
+Mean_Diff_Kriging_day<- Mean_Diff_Kriging_hr/24
+Mean_Diff_Kriging_yr<- Mean_Diff_Kriging_day/365
+
+Mean_Diff_AKMCS_hr<- Mean_Diff_AKMCS/3600
+Mean_Diff_AKMCS_day<- Mean_Diff_AKMCS_hr/24
+Mean_Diff_AKMCS_yr<- Mean_Diff_AKMCS_day/365
+
+#print(Mean_Time_Sobol_Best_hr)
+#print(Mean_Diff_BASS_hr)
+#print(Mean_Diff_Kriging_hr)
+#print(Mean_Diff_AKMCS_hr)
+
+min_times<- rep(NA,length(whichSobolBest))
+min_method<- rep(NA,length(whichSobolBest))
+
+for(i in 1:length(whichSobolBest)){
+  min_times[i]<- min(Mean_Time_BASS[i],Mean_Time_AKMCS[i],Mean_Time_Kriging[i])
+  if(min_times[i]==Mean_Time_BASS[i]) min_method[i]<- "BASS"
+  if(min_times[i]==Mean_Time_Kriging[i]) min_method[i]<- "Kriging"
+  if(min_times[i]==Mean_Time_AKMCS[i]) min_method[i]<- "AKMCS"
+}
+
+min_time
